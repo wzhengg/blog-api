599798aac16d3d3c72ecf1269222a08b9a56c230
William Zheng <williamzheng2001@gmail.com>
2022-12-27 15:40:18 -0500

    Use posts router

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const postsRouter = require('./routes/posts');
 
 const app = express();
 
@@ -26,5 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
 module.exports = app;
