b337b7ffe03acc280b7523260a2e257758998313
William Zheng <williamzheng2001@gmail.com>
2022-12-28 17:29:51 -0500

    Implement GET route handler for all comments

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,10 +1,29 @@
+const async = require('async');
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 
-exports.commentsGET = (req, res) => {
-  res.send(
-    `${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`
-  );
+exports.commentsGET = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postid);
+
+    if (!post) {
+      return res
+        .status(404)
+        .json({ error: `Could not find post with id ${req.params.postid}` });
+    }
+
+    const tasks = [];
+    for (const commentid of post.comments) {
+      tasks.push(function (callback) {
+        Comment.findById(commentid, callback);
+      });
+    }
+
+    const comments = await async.parallel(tasks);
+    res.json(comments);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
 exports.commentGET = (req, res) => {
