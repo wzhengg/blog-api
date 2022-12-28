ef487e5e0799a012e25b044505ff5b4741e1fa43
William Zheng <williamzheng2001@gmail.com>
2022-12-28 16:47:21 -0500

    Implement POST route handler for creating comment

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,3 +1,6 @@
+const Comment = require('../models/comment');
+const Post = require('../models/post');
+
 exports.commentsGET = (req, res) => {
   res.send(
     `${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`
@@ -10,10 +13,26 @@ exports.commentGET = (req, res) => {
   );
 };
 
-exports.commentPOST = (req, res) => {
-  res.send(
-    `${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`
-  );
+exports.commentPOST = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.postid);
+
+    if (!post) {
+      return res
+        .status(404)
+        .json({ error: `Could not find post with id ${req.params.postid}` });
+    }
+
+    const commentDoc = new Comment({ message: req.body.message });
+    const comment = await commentDoc.save();
+
+    post.comments.push(comment);
+    post.save();
+
+    res.send('Created comment');
+  } catch (err) {
+    res.json({ error: err.message });
+  }
 };
 
 exports.commentPUT = (req, res) => {
