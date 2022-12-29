ceb858bebc6b094718773cdbc1443a6adbdf2f12
William Zheng <williamzheng2001@gmail.com>
2022-12-29 12:15:49 -0500

    Validate params in GET route handler req

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -46,42 +46,46 @@ exports.commentsGET = [
   },
 ];
 
-exports.commentGET = async (req, res) => {
-  const { postid, commentid } = req.params;
-  try {
-    if (!isValidObjectId(postid)) {
-      return res
-        .status(404)
-        .json({ error: `Could not find post with id ${postid}` });
+exports.commentGET = [
+  // Validate param fields
+  param('postid').exists().isMongoId(),
+  param('commentid').exists().isMongoId(),
+
+  async (req, res) => {
+    const { postid, commentid } = req.params;
+    try {
+      // Find validation errors
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        // There are validation errors
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const post = await Post.findById(postid);
+
+      if (!post) {
+        // Didn't find post with given id
+        return res
+          .status(404)
+          .json({ error: `Could not find post with id ${postid}` });
+      }
+
+      const comment = await Comment.findById(commentid);
+
+      if (!comment || !post.comments.includes(comment._id)) {
+        // Didn't find comment with given id, or post doesn't include comment
+        return res.status(404).json({
+          error: `Could not find comment with id ${commentid} in post with id ${postid}`,
+        });
+      }
+
+      return res.json(comment);
+    } catch (err) {
+      return res.status(500).json({ error: err.message });
     }
-
-    if (!isValidObjectId(commentid)) {
-      return res
-        .status(404)
-        .json({ error: `Could not find comment with id ${commentid}` });
-    }
-
-    const post = await Post.findById(postid);
-
-    if (!post) {
-      return res
-        .status(404)
-        .json({ error: `Could not find post with id ${postid}` });
-    }
-
-    const comment = await Comment.findById(commentid);
-
-    if (!comment || !post.comments.includes(comment._id)) {
-      return res.status(404).json({
-        error: `Could not find comment with id ${commentid} in post with id ${postid}`,
-      });
-    }
-
-    return res.json({ comment });
-  } catch (err) {
-    return res.status(500).json({ error: err.message });
-  }
-};
+  },
+];
 
 exports.commentPOST = async (req, res) => {
   const { postid } = req.params;
