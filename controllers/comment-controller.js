c69a5ac43203568c80bee31533ca7b2eceb37303
William Zheng <williamzheng2001@gmail.com>
2022-12-29 12:33:32 -0500

    Validate param and body in PUT route handler req

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -131,45 +131,55 @@ exports.commentPOST = [
   },
 ];
 
-exports.commentPUT = async (req, res) => {
-  const { postid, commentid } = req.params;
-  try {
-    if (!isValidObjectId(postid)) {
-      return res
-        .status(404)
-        .json({ error: `Could not find post with id ${postid}` });
+exports.commentPUT = [
+  // Validate param fields
+  param('postid').exists().isMongoId(),
+  param('commentid').exists().isMongoId(),
+
+  // Validate and sanitize body field
+  body('message', 'Message is required').trim().notEmpty().escape(),
+
+  async (req, res) => {
+    const { postid, commentid } = req.params;
+    const { message } = req.body;
+
+    try {
+      // Find validation errors
+      const errors = validationResult(req);
+
+      if (!errors.isEmpty()) {
+        // There are validation errors
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const post = await Post.findById(postid);
+
+      if (!post) {
+        // Didn't find post with given id
+        return res
+          .status(404)
+          .json({ error: `Could not find post with id ${postid}` });
+      }
+
+      const comment = await Comment.findById(commentid);
+
+      if (!comment || !post.comments.includes(comment._id)) {
+        // Didn't find comment with given id, or post doesn't include comment
+        return res.status(404).json({
+          error: `Could not find comment with id ${commentid} in post with id ${postid}`,
+        });
+      }
+
+      // No validation errors and post and comment exist, update and save comment
+      comment.message = message;
+      await comment.save();
+
+      return res.send('Updated comment');
+    } catch (err) {
+      return res.status(500).json({ error: err.message });
     }
-
-    if (!isValidObjectId(commentid)) {
-      return res
-        .status(404)
-        .json({ error: `Could not find comment with id ${commentid}` });
-    }
-
-    const post = await Post.findById(postid);
-
-    if (!post) {
-      return res
-        .status(404)
-        .json({ error: `Could not find post with id ${postid}` });
-    }
-
-    const comment = await Comment.findById(commentid);
-
-    if (!comment || !post.comments.includes(comment._id)) {
-      return res.status(404).json({
-        error: `Could not find comment with id ${commentid} in post with id ${postid}`,
-      });
-    }
-
-    comment.message = req.body.message;
-    await comment.save();
-
-    return res.send('Updated comment');
-  } catch (err) {
-    return res.status(500).json({ error: err.message });
-  }
-};
+  },
+];
 
 exports.commentDELETE = async (req, res) => {
   const { postid, commentid } = req.params;
