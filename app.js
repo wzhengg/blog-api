ce18caca73a266af111da50e47eb2247f3ef1d77
William Zheng <williamzheng2001@gmail.com>
2022-12-28 21:32:22 -0500

    Update import

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const authorsRouter = require('./routes/authors');
 const postsRouter = require('./routes/posts');
 
 const app = express();
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/authors', authorsRouter);
 app.use('/posts', postsRouter);
 
 module.exports = app;
