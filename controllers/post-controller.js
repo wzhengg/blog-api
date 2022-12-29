541c9def9933f7f1788f01a07977d2102057a197
William Zheng <williamzheng2001@gmail.com>
2022-12-28 20:50:35 -0500

    Rewrite route handlers with async/await syntax

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -1,53 +1,86 @@
+const { isValidObjectId } = require('mongoose');
 const Post = require('../models/post');
 
-exports.postsGET = (req, res) => {
-  Post.find({ published: true }, (err, posts) => {
-    if (err) {
-      res.status(500).send('Error retrieving documents from database');
-    }
-    res.json(posts);
-  });
+exports.postsGET = async (req, res) => {
+  try {
+    const posts = await Post.find({ published: true });
+    return res.json(posts);
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
 };
 
-exports.postGET = (req, res) => {
-  Post.findById(req.params.postid, (err, post) => {
-    if (err) {
-      res.status(404).send(`Could not find post with id ${req.params.postid}`);
+exports.postGET = async (req, res) => {
+  const { postid } = req.params;
+  try {
+    const post = await Post.findById(postid);
+
+    if (!post) {
+      return res
+        .status(404)
+        .json({ error: `Could not find post with id ${postid}` });
     }
-    res.json(post);
-  });
+
+    return res.json(post);
+  } catch (err) {
+    if (!isValidObjectId(postid)) {
+      return res
+        .status(404)
+        .json({ error: `Could not find post with id ${postid}` });
+    }
+    return res.status(500).json({ error: err.message });
+  }
 };
 
-exports.postPOST = (req, res) => {
-  const postDoc = new Post({ title: req.body.title, body: req.body.body });
-  postDoc.save((err) => {
-    if (err) {
-      res.status(500).send('Error creating post');
-    }
-    res.send('Created post');
-  });
+exports.postPOST = async (req, res) => {
+  const { title, body } = req.body;
+  try {
+    const postDoc = new Post({ title, body });
+    await postDoc.save();
+    return res.send('Created post');
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
 };
 
-exports.postPUT = (req, res) => {
-  const post = new Post({
-    _id: req.params.postid,
-    title: req.body.title,
-    body: req.body.body,
-  });
+exports.postPUT = async (req, res) => {
+  const { postid } = req.params;
+  const { title, body } = req.body;
+  try {
+    const post = await Post.findById(postid);
 
-  Post.findByIdAndUpdate(req.params.postid, post, (err) => {
-    if (err) {
-      res.status(404).send(`Could not find post with id ${req.params.postid}`);
+    if (!post) {
+      return res
+        .status(404)
+        .send({ error: `Could not find post with id ${postid}` });
     }
+
+    post.title = title;
+    post.body = body;
+    await post.save();
+
     res.send('Updated post');
-  });
+  } catch (err) {
+    if (!isValidObjectId(postid)) {
+      return res
+        .status(404)
+        .send({ error: `Could not find post with id ${postid}` });
+    }
+    res.status(500).json({ error: err.message });
+  }
 };
 
-exports.postDELETE = (req, res) => {
-  Post.findByIdAndDelete(req.params.postid, (err) => {
-    if (err) {
-      res.status(404).send(`Could not find post with id ${req.params.postid}`);
-    }
+exports.postDELETE = async (req, res) => {
+  const { postid } = req.params;
+  try {
+    await Post.findByIdAndDelete(postid);
     res.send('Deleted post');
-  });
+  } catch (err) {
+    if (!isValidObjectId(postid)) {
+      return res
+        .status(404)
+        .send({ error: `Could not find post with id ${postid}` });
+    }
+    return res.status(500).json({ error: err.message });
+  }
 };
