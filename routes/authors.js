c171ca589719b46a405dd53b3183d2d2f61db0f3
William Zheng <williamzheng2001@gmail.com>
2022-12-28 21:50:32 -0500

    Create POST route for creating an author

--- routes/authors.js
+++ routes/authors.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 const authorController = require('../controllers/author-controller');
 
-// GET request for a specific user
+// GET request for a specific author
 router.get('/:authorid', authorController.authorGET);
+
+// POST request for creating an author
+router.post('/', authorController.authorPOST);
 
 module.exports = router;
