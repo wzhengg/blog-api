86219ea67a60fd44cdc7d4b1614d54b8759a2d63
William Zheng <williamzheng2001@gmail.com>
2022-12-29 22:05:27 -0500

    Configure JWT authentication

--- passport.js
+++ passport.js
@@ -1,5 +1,7 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const JwtStrategy = require('passport-jwt').Strategy;
+const { ExtractJwt } = require('passport-jwt');
 
 const Author = require('./models/author');
 
@@ -10,7 +12,7 @@ passport.use(
       const author = await Author.findOne({ username });
 
       if (!author) {
-        // Didn't find user
+        // Didn't find author
         return done(null, false, { message: 'Incorrect username or password' });
       }
 
@@ -26,4 +28,28 @@ passport.use(
       return done(err);
     }
   })
+);
+
+// Configure JWT authentication
+passport.use(
+  new JwtStrategy(
+    {
+      secretOrKey: process.env.TOKEN_SECRET,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    },
+    async (jwtPayload, done) => {
+      try {
+        const author = await Author.findById(jwtPayload.author._id);
+
+        if (!author) {
+          // Didn't find author
+          return done(null, false);
+        }
+
+        return done(null, author);
+      } catch (err) {
+        return done(err, false);
+      }
+    }
+  )
 );
