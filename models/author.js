30ec41c08648fb3d20d816f3ce3aa92737f45a4f
William Zheng <williamzheng2001@gmail.com>
2022-12-29 21:34:46 -0500

    Hash password before saving to db

--- models/author.js
+++ models/author.js
@@ -1,9 +1,22 @@
+const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+
+const SALT_ROUNDS = 10;
 
 const authorSchema = new Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
 });
+
+authorSchema.pre('save', async function (next) {
+  const hashedPassword = await bcrypt.hash(this.password, SALT_ROUNDS);
+  this.password = hashedPassword;
+  next();
+});
+
+authorSchema.methods.isValidPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 module.exports = mongoose.model('Author', authorSchema);
